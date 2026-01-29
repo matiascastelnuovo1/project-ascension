45e3e699e14adcc1ce5e1c25d3ff6ffe8ec9f8b3
matiascastelnuovo1 <matias.castelnuovo1@gmail.com>
2026-01-28 23:05:10 -0300

    WIP: cambio de mascara y plataformas

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,14 +1,12 @@
 extends Node
 
-enum ESTADO_MASCARA {VERDE,ROJO,AMARILLO}
-var estado_mascara_actual = ESTADO_MASCARA.VERDE
 var mascaras_agarradas = 0
+enum ESTADO_MASCARA {SOL,LUNA}
+var estado_mascara_actual = ESTADO_MASCARA.SOL
 
 func cambiar_mascara():
 	match estado_mascara_actual:
-		ESTADO_MASCARA.VERDE:
-			estado_mascara_actual = ESTADO_MASCARA.ROJO
-		ESTADO_MASCARA.ROJO:
-			estado_mascara_actual = ESTADO_MASCARA.AMARILLO
-		ESTADO_MASCARA.AMARILLO:
-			estado_mascara_actual = ESTADO_MASCARA.VERDE
+		ESTADO_MASCARA.SOL:
+			estado_mascara_actual = ESTADO_MASCARA.LUNA
+		ESTADO_MASCARA.LUNA:
+			estado_mascara_actual = ESTADO_MASCARA.SOL
--- scripts/personaje.gd
+++ scripts/personaje.gd
@@ -2,74 +2,79 @@ extends CharacterBody2D
 
 @export var base_speed = 200.0
 @export var  base_jump_velocity = -600
-var speed = base_speed
-var jump_velocity = base_jump_velocity
 @export var gravity := 1200.0
 @export var mascara_actual = Global.estado_mascara_actual
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var speed = base_speed
+var jump_velocity = base_jump_velocity
+var current_extra_layer: int = 2
 var animated_sprite_2d
 
 func _ready():
-	animated_sprite_2d = $AnimatedSprite2D
 	add_to_group("pers_jugable")
 
 func _physics_process(delta):
 	# Aplicar gravedad
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	else:
-		# Saltar automáticamente al tocar el suelo
-		velocity.y = jump_velocity
+	#else:
+		## Saltar automáticamente al tocar el suelo
+		#velocity.y = jump_velocity
 
 	# Movimiento horizontal
 	var direction := Input.get_axis("moverse_izquierda", "moverse_derecha")
 	velocity.x = direction * speed
 	
+	if Input.is_action_just_pressed("salto") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	# Flip sprite
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:		
+		animated_sprite.flip_h = true
+	
 	if Input.is_action_just_pressed("cambiar_mascara"):
 		Global.cambiar_mascara()
 		controlador_animacion_mascara()
 	
-	actualizar_stats()
+	#actualizar_stats()
 	move_and_slide()
 
 func controlador_animacion_mascara():
-
 	match Global.estado_mascara_actual:
-		Global.ESTADO_MASCARA.AMARILLO:
-			animated_sprite_2d.play("amarillo")
-		Global.ESTADO_MASCARA.ROJO:
-			animated_sprite_2d.play("rojo")
-		Global.ESTADO_MASCARA.VERDE:
-			animated_sprite_2d.play("verde")
+		Global.ESTADO_MASCARA.SOL:
+			animated_sprite.play("Sol")
+		Global.ESTADO_MASCARA.LUNA:
+			animated_sprite.play("Luna")
 
 func actualizar_stats():
 	match Global.estado_mascara_actual:
-		Global.ESTADO_MASCARA.VERDE:
+		Global.ESTADO_MASCARA.SOL:
 			speed = base_speed
 			jump_velocity = base_jump_velocity
-		Global.ESTADO_MASCARA.ROJO:
+		Global.ESTADO_MASCARA.LUNA:
 			speed = base_speed * 1.6
 			jump_velocity = base_jump_velocity * 1.6
-		Global.ESTADO_MASCARA.AMARILLO:
-			speed = base_speed * 0.8
-			jump_velocity = base_jump_velocity * 0.8
+		#Global.ESTADO_MASCARA.AMARILLO:
+			#speed = base_speed * 0.8
+			#jump_velocity = base_jump_velocity * 0.8
 			
-
-var current_extra_layer: int = 0  # 0 = ninguno, 2 o 3
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("cambiar_mascara"):
-		toggle_collision_layer()
+		cambiar_collision_layer()
 
-func toggle_collision_layer() -> void:
-	# Reset: siempre layer 1
+func cambiar_collision_layer() -> void:
+	# Reset: siempre layer 2
 	collision_mask = 1
+	print("Antes venia con_", current_extra_layer)
 	
-	if current_extra_layer == 0:
-		current_extra_layer = 2
-	elif current_extra_layer == 2:
+	if current_extra_layer == 2:
 		current_extra_layer = 3
 	else:
-		current_extra_layer = 0
+		current_extra_layer = 2
 	
 	set_collision_mask_value(current_extra_layer, true)
 	print("Ahora colisionando con layers: 1 y ", current_extra_layer)

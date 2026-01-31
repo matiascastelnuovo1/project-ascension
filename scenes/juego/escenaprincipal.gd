a13d65e771110ab70f4c61374d91bd517ddc6692
matiascastelnuovo1 <matias.castelnuovo1@gmail.com>
2026-01-31 12:04:01 -0300

    Salto - caminar - cambioMascara - SonidoNiveles funcionando

--- scenes/juego/escenaprincipal.gd
+++ scenes/juego/escenaprincipal.gd
@@ -1,9 +1,17 @@
 extends Node2D
 
+@onready var fmod_musica_inicio_emitter: FmodEventEmitter2D = $Sonido/musicaInicio/FmodMusicaInicioEmitter2D
 @onready var fmod_musica_intermedia_emitter: FmodEventEmitter2D = $Sonido/musicaIntermedia/FmodMusicaIntermediaEmitter2D
+@onready var fmod_musica_cielo_emitter: FmodEventEmitter2D = $Sonido/musicaCielo/FmodMusicaCieloEmitter2D
 
+func _on_area_inicio_2d_body_entered(body: Node2D) -> void:
+	fmod_musica_inicio_emitter.play()
+	pass
 
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("entro")
+func _on_area_intermedia_2d_body_entered(body: Node2D) -> void:
 	fmod_musica_intermedia_emitter.play()
+	pass
+
+func _on_area_cielo_2d_body_entered(body: Node2D) -> void:
+	fmod_musica_cielo_emitter.play()
+	pass

bbae1497dc2953d5cf2866832aae2a6710cc9c5c
matiascastelnuovo1 <matias.castelnuovo1@gmail.com>
2026-02-01 02:07:55 -0300

    Multiples arreglos. Cambio de plataformas en cielo por error de colisiones. Cambios esteticos menores en tutorial y ubicacion de condores.

--- scripts/swapCielo.gd
+++ scripts/swapCielo.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var fondo_luna: TileMapLayer = $Fondo/TileMapLayer
+@onready var fondo_luna: TileMapLayer = $FondoLuna
 
 func _ready():
 	Global.cambio_mascara.connect(_on_cambio_mascara)

5942e1a5dcc126976c3e18e768ec7469f5559775
matiascastelnuovo1 <matias.castelnuovo1@gmail.com>
2026-02-04 23:51:54 -0300

    cambios menores

--- scenes/juego/escenaprincipal.gd
+++ scenes/juego/escenaprincipal.gd
@@ -6,18 +6,13 @@ extends Node2D
 @onready var collision_inicio_shape: CollisionShape2D = $Sonido/MusicaInicio/AreaInicio2D/CollisionInicioShape2D
 @onready var collision_intermedia_shape: CollisionShape2D = $Sonido/MusicaIntermedia/AreaIntermedia2D/CollisionIntermedioShape2D
 @onready var collision_cielo_shape: CollisionShape2D = $Sonido/MusicaCielo/AreaCielo2D/CollisionCieloShape2D
-
 # Agregue esto entre los corchetes
-var menu_pausa
+@onready var menu_pausa = $CanvasLayerMenuPausa/MenuPausa
 
 func _ready():
-	menu_pausa = $CanvasLayerMenuPausa/MenuPausa
 	hide_pause()
-#
 
-func _input(event: InputEvent) -> void:
-	print(event)
-	
+func _input(event: InputEvent) -> void:	
 	#Agregue esta funcion para pausar el juego
 	if Input.is_action_just_pressed("pause"):
 		show_pause()
@@ -26,6 +21,16 @@ func _input(event: InputEvent) -> void:
 		#Saque este codigo de aca y lo reemplace por otro
 		#get_tree().change_scene_to_file("res://scenes/juego/menu.tscn")
 		show_pause()
+
+func hide_pause():
+	#Añado a hide_pause esta parte de codigo entre 
+	get_tree().paused = false
+	menu_pausa.hide()
+
+#Añado este codigo aca para pausar el juego
+func show_pause():
+	get_tree().paused = true
+	menu_pausa.show()
 
 func _on_area_inicio_2d_body_entered(body: Node2D) -> void:
 	fmod_musica_inicio_emitter.play()
@@ -44,15 +49,4 @@ func _on_area_cielo_2d_body_entered(body: Node2D) -> void:
 	collision_cielo_shape.set_deferred("disabled", true)
 	pass
 
-func hide_pause():
-	#Añado a hide_pause esta parte de codigo entre #
-	get_tree().paused = false
-	#
-	menu_pausa.hide()
-
-	
-#Añado este codigo aca para pausar el juego
-func show_pause():
-	get_tree().paused = true
-	menu_pausa.show()
 #
